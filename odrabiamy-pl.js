89dcf996fcfe6f12bc843ef4283f54464df28f8f
dommilosz <dommilosz@gmail.com>
2020-04-30 20:59:12 +0200

    upload

--- odrabiamy-pl.js
+++ odrabiamy-pl.js
@@ -20,7 +20,7 @@ async function login(email, password)
         try{
         browser = await puppeter.launch({ executablePath: 'chromium-browser' })}catch{
             puppeeteer = require('puppeteer-core');
-            browser = await puppeeteer.launch({
+            browser = await puppeter.launch({
                 'args': [
                     '--disable-web-security',
                     '--allow-http-screen-capture',
